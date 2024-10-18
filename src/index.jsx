affaa4803553e83f27d017e7ce26980fe90ca170
jam860 <jam860@uw.edu>
2024-10-17 20:34:11 -0700

    yay firebase works

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmYSO8nYMz_7O3cfjZScNYdB-QhCxNy5I",
+  authDomain: "mindconnect-af628.firebaseapp.com",
+  projectId: "mindconnect-af628",
+  storageBucket: "mindconnect-af628.appspot.com",
+  messagingSenderId: "899239528503",
+  appId: "1:899239528503:web:828ad75ad334288308d7b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
