3556f97b42616e671e23cda312122d61eebb4995
jam860 <jam860@uw.edu>
2024-10-24 13:28:08 -0700

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 // import { Quiz } from "./components/quiz"
 // import { Map } from "./components/map"
 import { Navbar } from "./components/Navbar"
+import { Footer } from "./components/Footer";
 import { Home } from "./components/home"
 import { Routes, Route, Navigate } from 'react-router-dom';
 
@@ -17,10 +18,11 @@ function App() {
       <p>The current count is {count}</p>
       <button onClick={() => setCount(prev => prev + 1)}>Click me</button>
       <p>Test that this builds correctly again</p> */}
-      {/* <Navbar /> */}
+      <Navbar />
         <Routes>
          <Route path="*" element={<Home />} />
         </Routes>
+      <Footer />
     </div>
   )
 }
