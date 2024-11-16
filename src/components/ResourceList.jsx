995ebe85c3e095f1416a9b48a6e3472b103df2eb
jam860 <jam860@uw.edu>
2024-11-16 02:01:12 -0800

    changes to buttons for resources

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -14,15 +14,15 @@ import { Link } from "react-router-dom";
 
 export function ResourceList(props) {
 
-    const [tagFilter, setTagFilter] = useState('All')
-    const [searchFilter, setSearchFilter] = useState('')
+    const [tagFilter, setTagFilter] = useState('All');
+    const [searchFilter, setSearchFilter] = useState('');
 
     function applyTagFilter(tag) {
-        setTagFilter(tag)
+        setTagFilter(tag);
     }
 
     function applySearchFilter(search) {
-        setSearchFilter(search)
+        setSearchFilter(search);
     }
 
     //REPLACE TEMP VALUE WHEN JSON USED 
@@ -52,11 +52,12 @@ export function ResourceList(props) {
         // OVERALL BACKGROUND STYLE HERE
         <div className="container my-4">
             <h1>Resource List</h1>
+            {!props.user && <p>You currently aren't signed in. <Link to="/login">Log-in</Link> to save these resources for later.</p>  }
             <CardFilter applyTagFilterCallback={applyTagFilter} applySearchFilterCallback={applySearchFilter}></ CardFilter>
             
             {/* ROW/COLUMN STYLE HERE */}
             <div>
-                <ResourceCardList resources={searchFilteredResources} />
+                <ResourceCardList resources={searchFilteredResources} user={props.user} saveResource={props.saveResource} savedResources={props.savedResources} deleteResource={props.deleteResource} />
             </div>
         </div>
     )
@@ -64,30 +65,52 @@ export function ResourceList(props) {
 
 
 function ResourceCard(props) {
-    const resource = props.resource
+
+    const [confirmation, setConfirmation] = useState("");
+
+    const resource = props.resource;
+
+    let saveButton = (<button className="btn btn-success action-button flex-fill" onClick={() => {
+                            props.saveResource(props.keyVal);
+                            setConfirmation(<p>This resource has been successfully saved to your <Link to="/user">profile</Link>.</p>);
+                        }}>Save Resource</button>);
+
+    if (props.user && props.savedResources != null) {
+        props.savedResources.forEach((resource) => {
+            if (resource.resourceNum == props.keyVal) {
+                saveButton = <button className="btn btn-danger action-button flex-fill" onClick={() => {
+                    props.deleteResource(props.keyVal);
+                    setConfirmation(<p>This resource has been successfully removed from your <Link to="/user">profile</Link>.</p>);
+                }}>Remove Saved Resource</button>
+            }
+        })
+    } else if (!props.user) {
+        saveButton = (<p>Want to save this resource for later? <Link to="/login">Sign-in</Link> to save it!</p>);
+    }
+
+    
     return (
-        <div className="card mb-5" style={{ width: '20rem'}}>
-            <img src={resource.properties.image} className="card-img-top" alt="..." />
+        <div className="mb-5" style={{ width: '20rem'}}>
+            <div className="card" style={{ width: '20rem'}}>
+                <img src={resource.properties.image} className="card-img-top" alt="..." />
 
-            <div className="card-body" style={{ fontSize: '1.2rem' }}>
-                <h2 className="card-title" style={{ fontSize: '1.2rem' }}>{resource.properties.resourceName}</h2>
-                {/* <p className="card-text">{resource.properties.description}</p> */}
+                <div className="card-body" style={{ fontSize: '1.2rem' }}>
+                    <h2 className="card-title" style={{ fontSize: '1.2rem' }}>{resource.properties.resourceName}</h2>
+                </div>
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{"Address: " + resource.properties.address}</li>
+                    <li className="list-group-item">{"Phone: " + resource.properties.phoneNumber}</li>
+                    <li className="list-group-item">
+                        <a href={resource.properties.website} className="card-link">{resource.properties.website}</a>
+                    </li>
+                    <div className="d-flex">
+                        <Link className="btn btn-primary learn-more flex-fill" to={"/resources/" + props.keyVal}>Learn More</Link>
+                        {props.user && saveButton}
+                    </div>
+                </ul>
             </div>
-
-            <ul className="list-group list-group-flush">
-                {/* <li className="list-group-item">
-                    <ul className="list-group list-group">
-                        {tagsList}
-                    </ul>
-                </li> */}
-                <li className="list-group-item">{"Address: " + resource.properties.address}</li>
-                <li className="list-group-item">{"Phone: " + resource.properties.phoneNumber} </li>
-                <li className="list-group-item">
-                    <a href={resource.properties.website} className="card-link">{resource.properties.website}</a>
-                </li>
-                <Link className="btn btn-primary learn-more" to={"/resources/" + props.keyVal}>Learn More</Link>
-            </ul>
-
+            {confirmation != "" && confirmation}
         </div>
     )
 }
@@ -98,7 +121,7 @@ function ResourceCardList(props) {
     const resourceCardList = resources.map((resource, index) => {
         return (
             <div className="justify-content-center col-md-4 col-sm-6" key={index}>
-                <ResourceCard resource={resource} keyVal={index} key={index} />
+                <ResourceCard resource={resource} keyVal={index} key={index} user={props.user} saveResource={props.saveResource} savedResources={props.savedResources} deleteResource={props.deleteResource}/>
             </div>
         );
     })
