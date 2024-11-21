2363cf93872a98bba6338fa586914d6ee59a756a
jam860 <jam860@uw.edu>
2024-11-20 17:47:46 -0800

    added scroll to top component

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
+import ScrollToTop from "./components/ScrollToTop.jsx"
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
@@ -27,6 +28,7 @@ const app = initializeApp(firebaseConfig);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
   </StrictMode>,
